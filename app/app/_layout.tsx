643c9b834f9548c9b5b70e0b40259cf976d79662
indresh404 <indreshsuresh95@gmail.com>
2026-04-14 01:30:29 +0530

    added folder structure & welcome page

---
+++ app/app/_layout.tsx
@@ -0,0 +1,36 @@
+// app/_layout.tsx
+import {
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+
+export default function RootLayout() {
+  const [loaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+    Poppins_300Light,
+  });
+
+  if (!loaded) return null;
+
+  return (
+    <>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(auth)" />
+        <Stack.Screen name="(onboarding)" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+      <StatusBar style="light" />
+    </>
+  );
+}
